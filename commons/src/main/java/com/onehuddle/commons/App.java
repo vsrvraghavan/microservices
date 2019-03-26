9116a804b30daeed40b8027c38c9289a501d5ee5
VSRV Raghavan <vsrv.raghavan@gmail.com>
2019-03-26 13:11:14 +0530

    deleted target files

--- commons/src/main/java/com/onehuddle/commons/App.java
+++ commons/src/main/java/com/onehuddle/commons/App.java
@@ -1,13 +1,40 @@
 package com.onehuddle.commons;
 
+import java.util.LinkedHashMap;
+
+import com.onehuddle.commons.utils.KiteConnectUtil;
+import com.onehuddle.commons.utils.Nse;
+
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+	
+	
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        
+    	
+    		new KiteConnectUtil();
+    	   
+    		/*
+    		Nse nse = new Nse();
+    	   
+    	   
+           
+    	   LinkedHashMap<String, String> hashMap = nse.getStockDetails("INFY");
+
+           System.out.println(hashMap.get(Nse.LAST_TRADED_PRICE));
+           System.out.println(hashMap.get(Nse.PREVIOUS_CLOSE));
+           System.out.println(hashMap.get(Nse.OPEN));
+           System.out.println(hashMap.get(Nse.HIGH));
+           System.out.println(hashMap.get(Nse.LOW));
+           System.out.println(hashMap.get(Nse.CLOSE));
+           */
+           
+        
     }
 }
